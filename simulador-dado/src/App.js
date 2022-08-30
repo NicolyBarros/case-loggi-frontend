97f845e3b432e08b3b88f9fc7be6970fcbb8cdd8
Nicoly Barros <nicolybarros00@gmail.com>
2022-08-29 21:54:37 -0300

    Iniciado estilizacao.

--- simulador-dado/src/App.js
+++ simulador-dado/src/App.js
@@ -1,58 +1,66 @@
+import React from "react";
+import "./App.css"
 import { useState } from "react";
-import './App.css';
-import Dado1 from "./images/face1.png"
-import Dado2 from "./images/face2.png"
-import Dado3 from "./images/face3.png"
-import Dado4 from "./images/face4.png"
-import Dado5 from "./images/face5.png"
-import Dado6 from "./images/face6.png"
+import Dado1 from "./images/face1.png";
+import Dado2 from "./images/face2.png";
+import Dado3 from "./images/face3.png";
+import Dado4 from "./images/face4.png";
+import Dado5 from "./images/face5.png";
+import Dado6 from "./images/face6.png";
+import { TitleProject, BodyApp, ImageDado, Button, ImageDadoTitle, SectionTitle } from "./styleApp"
 
 function App() {
 
   const [sorteio, setSorteio] = useState(1)
+  const [jogando, setJogando] = useState(false)
 
   const jogar = () => {
     const sortearNum = Math.floor(Math.random() * 6 + 1)
 
     setSorteio(sortearNum)
+    setJogando(true)
+
+    setTimeout(() => {
+      setJogando(false)
+    }, 600)
+
   }
+
 
   const renderizaDado = () => {
     switch (sorteio) {
       case 1:
-        return <img src={Dado1} alt="Imagem da face do dado com 1 ponto"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado1} alt="Imagem da face do dado com 1 ponto" />
       case 2:
-        return <img src={Dado2} alt="Imagem da face do dado com 2 pontos"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado2} alt="Imagem da face do dado com 2 pontos" />
       case 3:
-        return <img src={Dado3} alt="Imagem da face do dado com 3 pontos"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado3} alt="Imagem da face do dado com 3 pontos" />
       case 4:
-        return <img src={Dado4} alt="Imagem da face do dado com 4 pontos"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado4} alt="Imagem da face do dado com 4 pontos" />
       case 5:
-        return <img src={Dado5} alt="Imagem da face do dado com 5 pontos"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado5} alt="Imagem da face do dado com 5 pontos" />
       case 6:
-        return <img src={Dado6} alt="Imagem da face do dado com 6 pontos"/>
-        break
+        return <ImageDado className={`${jogando && 'dado-rolando'}`} src={Dado6} alt="Imagem da face do dado com 6 pontos" />
       default:
         return <p>Erro ao simular dado.</p>
     }
   }
 
   return (
-    <div className="App">
+    <BodyApp>
 
-      <h1>Simulador de dado</h1>
+      <SectionTitle>
+        <ImageDadoTitle src="https://img.freepik.com/vetores-premium/objeto-3d-isolado-de-dados-para-jogos-de-azar-projetam-dados-de-cassino-e-mesa-de-poquer-ou-jogos-de-tabuleiro-cubo-branco-realista-com-cinco-pontos-pretos-ou-pips-ilustracao-vetorial_341509-3356.jpg?w=2000" />
+        <TitleProject>Simulador de dado</TitleProject>
+        <ImageDadoTitle src="https://img.freepik.com/vetores-premium/objeto-3d-isolado-de-dados-para-jogos-de-azar-projetam-dados-de-cassino-e-mesa-de-poquer-ou-jogos-de-tabuleiro-cubo-branco-realista-com-cinco-pontos-pretos-ou-pips-ilustracao-vetorial_341509-3356.jpg?w=2000" />
+      </SectionTitle>
 
       {renderizaDado()}
 
       <br />
 
-      <button onClick={jogar}>jogar</button>
-    </div>
+      <Button onClick={jogar}>{jogando ? "Jogando" : "Jogar"}</Button>
+    </BodyApp>
   );
 }
 
--- simulador-dado/src/styleApp.js
+++ simulador-dado/src/styleApp.js
@@ -1,3 +1,47 @@
 import styled from "styled-components"
 
-const TituloJogo
+export const SectionTitle = styled.section`
+    display:flex;
+    justify-items:center;
+    align-items: center;
+    justify-content:center;
+`
+
+export const TitleProject = styled.h1`
+    font-size: 5em;
+
+`
+
+export const ImageDadoTitle = styled.img`
+    height: 5em;
+    width: 5em;
+`
+
+export const BodyApp = styled.div`
+    text-align: center;
+    justify-items: center;
+    font-family: 'Roboto', sans-serif;
+`
+
+export const ImageDado = styled.img`
+    border-radius: 6px;
+    height: 10em;
+    width: 10em;
+    box-shadow: 0.3em 0.2em 0.2em grey;
+`
+
+export const Button = styled.button`
+    cursor: pointer;
+    width: 30vh;
+    height:6vh;
+    border-radius: 5px;
+    font-size:30px;
+    margin-top: 2em;
+    background-color: black;
+    color: white;
+
+    :hover{
+    background-color: white;
+    color: black;
+    }
+`
